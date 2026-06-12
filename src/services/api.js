fed833a2110905b5e417bcb73bdc111bf3096523
Inturi Kirthana <inturikirthana2808@gmail.com>
2026-06-12 21:54:12 +0530

    Updated production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://connect-via-skills-backend.onrender.com/api"
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
 
   const token = localStorage.getItem("token");
@@ -13,6 +14,7 @@ API.interceptors.request.use((req) => {
   }
 
   return req;
+
 });
 
 export default API;
